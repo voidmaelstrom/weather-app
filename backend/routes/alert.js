6ca41596ece5bbbb60cc715e5da90963c451861a
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-04-28 16:10:58 -0600

    Get initial routes added to fill in later

---
+++ backend/routes/alert.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const Location = require('../models/Alert');
+const router = express.Router();
+
---
+++ backend/routes/forecast.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const Location = require('../models/Forecast');
+const router = express.Router();
+
