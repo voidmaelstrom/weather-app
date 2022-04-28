6bbdce084ab90a0d64cdfd398fd9a69755a41d61
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-04-27 21:42:34 -0600

    Initial setup of backend

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+// import all libraries
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+
+require('dotenv').config();
+
+// setup connections
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to DB 🔌"))
+    .catch(() => console.log("Couldn't connect to db ❌"));
+
+// setup middlewares
+app.use(cors()); // CORS => Cross origin resource sharing; // share frontend <-> backend
+app.use(express.json());
+
+// setup routes
+app.get("/", (req, res) => {
+  return res.json({message: "API Success!"})
+})
+
+// start listening on server
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log("Server running...");
+})
