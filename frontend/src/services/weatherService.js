c10fc7b9dc41913b97ba994a2b79a63948d72a78
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-04-30 10:13:27 -0600

    Start on external API service

--- frontend/src/services/weatherService.js
+++ frontend/src/services/weatherService.js
@@ -1,3 +1,29 @@
 import axios from "axios";
 const apiUrl = process.env.EXTERNAL_API_URL;
+const apiKey = process.env.API_KEY;
+
+async function getCoords(){
+  pos = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+  })
+  return pos.coords.latitude, pos.coords.longitude
+}
+
+export const getMyLocation = async () => {
+  try {
+    const response = await axios.get(`${apiUrl}/services/timeline/${getCoords}?key=${apiKey}`);
+    return response;
+  } catch(error) {
+    if (error.response) { // get response with a status code not in range 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) { // no response
+      console.log(error.request);
+    } else { // Something wrong in setting up the request
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
+  }
+}
 
