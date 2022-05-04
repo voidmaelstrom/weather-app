35d5986ba85419ac9c8738ddedf1a7c7610a4f90
Hannah Gooding <hgooding6@gmail.com>
2022-05-04 18:19:04 -0500

    added 5 day component

--- backend/models/Forecast.js
+++ backend/models/Forecast.js
@@ -5,31 +5,24 @@ const ForecastSchema = Schema(
   {
     latitude: {
       Type: Number,
-      Required: true
     },
     longitude: {
       Type: Number,
-      Required: true
     },
     address: {
       Type: String,
-      Required: true
     },
     description: {
       Type: String,
-      Required: true
     },
       datetime: {
         Type: Number,
-        Required: true
       },
       temp: {
         Type: Number,
-        Required: true
       },
       feelslike: {
         Type: Number,
-        Required: true
       }
     
   }
