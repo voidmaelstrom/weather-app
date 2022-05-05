f8f6143a197e5b03e67f1c707f182a7e5552a225
Bradley Stouffer <voidmaelstrom@gmail.com>
2022-05-04 20:05:29 -0600

    Get Current.js to where it won't complain

--- frontend/src/components/Current.js
+++ frontend/src/components/Current.js
@@ -1,28 +1,33 @@
+import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import React, { useState, useEffect } from "react";
-const config = require('../config.json');
-const apiUrl = config.EXTERNAL_API_URL;
-const apiKey = config.API_KEY;
 
-
-const weatherUrl = `${apiUrl}/services/timeline/kansas%20city?unitGroup=us&include=current&key=${apiKey}`
+// const weatherUrl = `${apiUrl}/services/timeline/kansas%20city?unitGroup=us&include=current&key=${apiKey}`
 
 export default function Current () {
     const [weatherData, setWeatherData] = useState({});
 
     useEffect(() => {
-        getWeather();
+        getWeatherForecast();
     }, []);
 
-    const getWeather = async () => {
-        const response = await fetch(weatherUrl);
-        const jsonData = await response.json();
-        setWeatherData(jsonData)
-    };
-
-    console.log(weatherData.currentConditions);
+    const getWeatherForecast = () => {
+        const options = {
+            method: 'GET',
+            url: '/api/external/forecast',
+            params: {latLong: `${localStorage.getItem("lat")},${localStorage.getItem("long")}`,
+                     timeRange: ''},
+        }
+      
+        axios.request(options).then((response) => {
+            setWeatherData(response.data)
+      
+        }).catch((error) => {
+            console.error(error)
+        })
+    }
 
     return (
         <div className="current">
